36ab0b01089a16f03b831afd9662c860655c346e
ZacharyFredeman <zachary.fredeman1@marist.edu>
2022-10-12 20:29:05 -0400

    Fixed button

--- src/components/ButtonElements.js
+++ src/components/ButtonElements.js
@@ -6,7 +6,7 @@ export const ButtonSt = styled(LinkS)`
     border-radius: 50px;
     background: ${({primary}) => (primary ? '#f57e42' : '#010606')};
     white-space: nowrap;
-    paddding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
+    padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
     color: ${({dark}) => (dark ? '#010606' : '#fff')};
     font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
     outline: none;
@@ -27,7 +27,7 @@ export const ButtonRt = styled(LinkR)`
     border-radius: 50px;
     background: ${({primary}) => (primary ? '#f57e42' : '#010606')};
     white-space: nowrap;
-    paddding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
+    padding: ${({big}) => (big ? '14px 48px' : '12px 30px')};
     color: ${({dark}) => (dark ? '#010606' : '#fff')};
     font-size: ${({fontBig}) => (fontBig ? '20px' : '16px')};
     outline: none;
